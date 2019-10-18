4c9f3fe73ae0cc7b76b650642c0369c9df7169f1
geekxia <448914712@qq.com>
2019-10-18 11:03:47 +0800

    1018

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 ##项目运行
 
-```
 npm install
 npm install nodemon -g
 npm start
-```
--- server.js
+++ server.js
@@ -1,28 +1,33 @@
-var express = require('express');
-
+var express = require('express')
 var app = express()
-var http = require('http').createServer(app)
-app.use(express.static('public'))
+// 用http模块创建server
+var server = require('http').createServer(app)
+// 创建服务端socket
+var io=require('socket.io')(server)
 
-var io = require('socket.io')(http)
+var userNo = 0   // 用户ID，模拟用户名
 
-var userNo = 0
-
+// 监听客户端的socket连接
 io.on('connect', function(socket) {
+  // socket代表当前来请求连接的用户
   var user = (++userNo)
-  console.log(user + ' 上线了')
-  io.emit('msg_from_server', user + ' 上线了')
+  io.emit('msg-from-server', user + ':---------> 上线了')
 
-  socket.on('msg_from_client', function(data) {
-    console.log(user +' 发来消息：', data)
-    io.emit('msg_from_server', user + '说：' + data)
-  })
+  // 监听socket连接断开事件
   socket.on('disconnect', function() {
-    console.log(user + ' 下线了')
-    io.emit('msg_from_server', user + ' 下线了')
+    io.emit('msg-from-server', user + ':---------> 下线了')
+  })
+
+  // 监听客户端的消息，事件名可以自定义，接收客户端消息，并把消息发送给客户端
+  socket.on('msg-from-client', function(data) {
+    // 向客户端发消息
+    io.emit('msg-from-server', user + ': '+ data)
   })
 })
 
-http.listen(9999, function() {
-  console.log('server is running on 9999')
+// 静态资源目录
+app.use(express.static('public'))
+
+server.listen('8000', function() {
+  console.log('server is running on 8000')
 })
